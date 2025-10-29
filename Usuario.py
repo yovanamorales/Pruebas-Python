ec2c81d69159692b000a7d4ad859c560d130cd49
yovanamorales <yovana.morales.bellido@gmail.com>
2025-10-29 13:19:33 -0300

    Refactor Usuario class to use TarjetaCredito objects

--- Usuario.py
+++ Usuario.py
@@ -1,4 +1,4 @@
-from TarjetaCredito import TarjetaCredito
+from TarjetaCredito import *
 
 #Actualizar el método __init__ de la clase Usuario
 class Usuario:
@@ -9,48 +9,38 @@ class Usuario:
         self.email = email
         self.tarjetas = [] #lista TarjetaCredito tener varias tarjetas. 
 
-    #actualizar tu clase de Usuario con el objetivo de asociar la clase de TarjetaCredito. 
-    def agregar_tarjeta(self, limite_credito, intereses):
-        tarjeta = TarjetaCredito(limite_credito,intereses)
-        self.tarjetas.append(tarjeta)
-        print(f"{self.nombre}: limite Credito $ {limite_credito}, Taza de interes {intereses *100:.1f} %")
-        return self
-    
-    #Actualiza el método hacer_compra de la clase Usuario
-    def hacer_compra(self, monto,numero_tarjeta = 0):
-        if not self.tarjetas:
-            print("El usuario no tiene tarjeta registrada")
-            return self
-        if self.tarjetas:
-            self.tarjetas[numero_tarjeta].compra(monto)
+    def agregar_tarjetas(self, tarjeta):
+        if isinstance(tarjeta,TarjetaCredito):
+            self.tarjetas.append(tarjeta)
         else:
-            print(" Usuario tiene {len(self.tarjetas)} tarjeta(s).")
+            print("la tarjeta indicada no existe")
         return self
 
-    #Actualiza el método pagar_tarjeta de la clase Usuario
-    def pagar_tarjeta(self,monto,numero_tarjeta =0):
-        if not self.tarjetas:
-            print("No hay tarjeta que operar.")
-            return self
+    def hacer_compra(self, monto, tarjeta):
+        if tarjeta in self.tarjetas:
+            tarjeta.compra(monto)
+            print(f"{self.nombre}: Compra por ${monto:.2f} realizada con éxito en la tarjeta {tarjeta.numero}.")
+        else:
+            print("tarjeta no pertenece al usuario.")
+        return self
         
-        if 0 <= numero_tarjeta < len(self.tarjetas):
-            self.tarjetas[numero_tarjeta].pago(monto)
-        else:
-            print(f"El usuario tiene {len(self.tarjetas)} tarjeta(s).")
+    def pagar_tarjeta(self, monto, tarjeta):
+        if tarjeta in self.tarjetas:
+           tarjeta.pago(monto)
         return self
 
-    #Actualiza el método mostrar_saldo_usuario de la clase Usuario
     def mostrar_saldo_usuario(self):
-        print(f"Usuario: {self.nombre} {self.apellido}{self.email}")
-        if not self.tarjetas:
-            print("No hay tarjeta que operar.")
-            return self
-
-        for i, tarjeta in enumerate(self.tarjetas, start=1):
-            print(f"Tarjeta {i}:")
-            tarjeta.mostrar_info_tarjeta()
+        print(f"usuario:{self.nombre} {self.apellido},{self.email} Saldo a Pagar:")
+        print("Saldo de tarjetas:")
+        for tarjeta in self.tarjetas:
+             tarjeta.mostrar_info_tarjeta()
         return self
 
 
+
+master_card = TarjetaCredito(200000,0.03)
 usuario1= Usuario("yovana" , "Morales", "yovana@correo.com")
-usuario1.agregar_tarjeta(100000,0.05).hacer_compra(50000).pagar_tarjeta(10000).mostrar_saldo_usuario()
+usuario1.agregar_tarjetas(master_card)
+usuario1.hacer_compra(50000,master_card)
+usuario1.pagar_tarjeta(10000,master_card)
+usuario1.mostrar_saldo_usuario()
